59e1291acc522743b18cdd55c7bb2ec5c9db6038
ezgisutuncel <tuncelezgisu@gmail.com>
2022-07-19 13:32:05 +0300

    bootstrap keycloak

--- src/business/providers/__init__.py
+++ src/business/providers/__init__.py
@@ -1,5 +1,7 @@
 import os
 
+from business.providers.keycloak import ProviderKeycloak
+
 from .firebase import ProviderFirebase
 
 def get_provider():
@@ -7,5 +9,7 @@ def get_provider():
 
     if provider.upper() == 'FIREBASE':
         return ProviderFirebase()
+    elif provider.upper() == 'KEYCLOAK':
+        return ProviderKeycloak()
     else:
         raise Exception(f"Unknwon provider <{provider}>")
