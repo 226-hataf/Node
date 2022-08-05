c2daf6f1e5d0fd8f68f06b1f01e0e243aab36721
ezgisutuncel <tuncelezgisu@gmail.com>
2022-08-05 16:55:07 +0300

    implement password policy

--- src/business/providers/keycloak.py
+++ src/business/providers/keycloak.py
@@ -1,22 +1,41 @@
 import email
+
+import requests
 from core import log
 import os
 from business.models.users import User
 from business.providers.base import DuplicateEmailError, Provider
-from keycloak import KeycloakAdmin, KeycloakPostError
+from keycloak import KeycloakAdmin, KeycloakOpenID, KeycloakPostError
 
 
 class ProviderKeycloak(Provider):
     admin_user_created = None
 
     def __init__(self) -> None:
+        # self.access_token = KeycloakOpenID.token(self,
+        #     grant_type='authorization_code',
+        #     code='the_code_you_get_from_auth_url_callback',
+        #     redirect_uri="your_call_back_url")
+
+        # access token url: (post) f"{keycloak_url}/realms/{realm}/protocol/openid-connect/token"
+        # data = {
+        #     "grant_type": "client_credentials",
+        #     "client_id": os.environ.get('CLIENT_ID'),
+        #     "client_secret": os.environ.get('CLIENT_SECRET')
+        #     }
+        # headers = {
+        #     'Content-Type': 'application/x-www-form-urlencoded'
+        # }
+        # response = requests.post(f"{os.environ.get('KEYCLOAK_URL')}/{os.environ.get('REALM_NAME')}/clients-initial-access", data=data, headers=headers)
+        # self.access_token = response.json()['access_token']
 
         self.keycloak_admin = KeycloakAdmin(
             server_url="https://accounts.dev.zekoder.com",
             client_id=os.environ.get('CLIENT_ID'),
             realm_name=os.environ.get('REALM_NAME'),
-            client_secret_key=os.environ.get('SECRET')
+            client_secret_key=os.environ.get('SECRET') # access_token
         )
+        self.token = self.keycloak_admin.token
         super().__init__()
 
 
@@ -31,6 +50,15 @@ class ProviderKeycloak(Provider):
                 "lastname": "Account"
             }
             
+            length = f"length({int(os.environ.get('LENGTH'))})"
+            specialChars = f"specialChars({int(os.environ.get('SPECIAL_CHARACTERS'))})"
+            upperCase = f"upperCase({int(os.environ.get('UPPERCASE'))})"
+            digits = f"digits({int(os.environ.get('DIGITS'))})"
+
+            password_policy = {"passwordPolicy": f"{length} and {specialChars} and {upperCase} and {digits}"}
+
+            self.update_password_policy(password_policy)
+
             try:
                 self._create_user(**default_admin)
             except:
@@ -38,6 +66,13 @@ class ProviderKeycloak(Provider):
 
             ProviderKeycloak.admin_user_created = True
 
+
+    def update_password_policy(self, password_policy: dict) -> str:
+        try:
+            headers = {"Authorization": f"Bearer {self.token['access_token']}"}
+            response = requests.put(f"{os.environ.get('KEYCLOAK_URL')}/admin/realms/{os.environ.get('REALM_NAME')}", headers=headers, json=password_policy)
+        except Exception as e:
+            raise e
 
     def _create_user(self, email: str, username: str, firstname: str, lastname: str, enabled: bool=True) -> str:
         try:
