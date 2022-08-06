03a9a55a48fd324b5910d46458f144e0edb4c83f
ezgisutuncel <tuncelezgisu@gmail.com>
2022-08-06 11:59:54 +0300

    move the policy definition in to the update method

--- src/business/providers/keycloak.py
+++ src/business/providers/keycloak.py
@@ -34,14 +34,7 @@ class ProviderKeycloak(Provider):
                 "lastname": "Account"
             }
             
-            length = f"length({int(os.environ.get('LENGTH'))})"
-            specialChars = f"specialChars({int(os.environ.get('SPECIAL_CHARACTERS'))})"
-            upperCase = f"upperCase({int(os.environ.get('UPPERCASE'))})"
-            digits = f"digits({int(os.environ.get('DIGITS'))})"
-
-            password_policy = {"passwordPolicy": f"{length} and {specialChars} and {upperCase} and {digits}"}
-
-            self.update_password_policy(password_policy)
+            self.update_password_policy()
 
             try:
                 self._create_user(**default_admin)
@@ -51,9 +44,18 @@ class ProviderKeycloak(Provider):
             ProviderKeycloak.admin_user_created = True
 
 
-    def update_password_policy(self, password_policy: dict) -> str:
+    def update_password_policy(self) -> str:
         try:
+            # password policies
+            length = f"length({int(os.environ.get('LENGTH'))})"
+            specialChars = f"specialChars({int(os.environ.get('SPECIAL_CHARACTERS'))})"
+            upperCase = f"upperCase({int(os.environ.get('UPPERCASE'))})"
+            digits = f"digits({int(os.environ.get('DIGITS'))})"
+
+            # definiton of password policy
+            password_policy = {"passwordPolicy": f"{length} and {specialChars} and {upperCase} and {digits}"}
             headers = {"Authorization": f"Bearer {self.token['access_token']}"}
+
             response = requests.put(f"{os.environ.get('KEYCLOAK_URL')}/admin/realms/{os.environ.get('REALM_NAME')}", headers=headers, json=password_policy)
         except Exception as e:
             raise e
