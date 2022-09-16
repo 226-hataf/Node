95c816ee4373280f041aa296a0cf96877d71faf1
Abdul Rehman <ar.rehmanmirza@gmail.com>
2022-09-16 15:04:53 +0500

    update

--- src/redis_service/redis_service.py
+++ src/redis_service/redis_service.py
@@ -3,8 +3,7 @@ import os
 
 redi = redis.Redis(
     host=os.environ.get('REDIS_HOST', 'localhost'),
-    port=os.environ.get('REDIS_PORT', 6379),
-    password=os.environ.get('REDIS_PASSWORD'),
+    port=os.environ.get('REDIS_PORT', 6379)
 )
 
 ONE_HOUR_IN_SECONDS = 3600
