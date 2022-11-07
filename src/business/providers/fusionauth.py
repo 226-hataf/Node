e089799352f371205279396ea85e430b0cc14abb
abdul rehman <ar.rehmanmirza@cyberneticlabs.io>
2022-11-07 00:10:52 -0800

    list user

--- src/business/providers/fusionauth.py
+++ src/business/providers/fusionauth.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 from business.providers.base import *
 from fusionauth.fusionauth_client import FusionAuthClient
@@ -19,6 +20,37 @@ class ProviderFusionAuth(Provider):
             os.environ.get('FUSIONAUTH_URL')
         )
 
+    def list_users(self, page: str, page_size: int, search: str):
+        print('aaaa')
+        headers = {
+            'Authorization': '33678980-8dbf-4b5c-8f87-d7f258c6a5a1',
+        }
+        response = requests.get('https://accounts.dev.zekoder.net/api/user/search?queryString=*', headers=headers)
+        if response.status_code == 200:
+            res = response.json()
+            users_data = [self._cast_user(user) for user in res['users']]
+            next_page = int(page) + 1
+            return users_data, next_page, res['total']
+        else:
+            return [], 0, 0
+
+    def _cast_user(self, data: dict):
+        full_name = data.get('firstName', '')
+        if data.get('lastName', ''):
+            full_name = f"{full_name} {data.get('lastName', '')}"
+        created_at = datetime.datetime.fromtimestamp(data['insertInstant'] / 1000)
+
+        return User(
+            id=data['id'],
+            email=data['email'],
+            verified=data['verified'],
+            user_status=data['active'],
+            created_at=str(created_at).split(".")[0],
+            permissions=[],
+            roles=[],
+            full_name=full_name
+        )
+
     def login(self, user_info: str):
         self.setup_fusionauth()
         try:
@@ -31,7 +63,8 @@ class ProviderFusionAuth(Provider):
                 ip_address = requests.get('https://api64.ipify.org?format=json').json()
                 return {"data": response.success_response, "ip": ip_address["ip"]}
             else:
-                return "Criteria does not match !"
+                return { "e": response.error_response, "s": response.success_response }
+                # return "Criteria does not match !"
         except Exception as e:
             log.error(e)
             raise e
