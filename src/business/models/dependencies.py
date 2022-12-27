54676fd327739d66f84c6f95b79dc43a74f91e05
Abdul Rehman Mirza <ar.rehmanmirza@cyberneticlabs.io>
2022-12-27 16:01:00 +0500

    set session variable

--- src/business/models/dependencies.py
+++ src/business/models/dependencies.py
@@ -2,9 +2,10 @@ from typing import Optional
 from datetime import date as date_type
 from fastapi import Depends, HTTPException
 from fastapi.security import HTTPBearer
-
+from sqlalchemy.orm import Session
 from business.providers import get_provider
 from business.providers.base import Provider
+from config.db import get_db
 from core import log
 
 auth_schema = HTTPBearer()
@@ -23,15 +24,17 @@ class CommonDependencies:
 
 
 class ProtectedMethod:
-    def __init__(self, token: str = Depends(auth_schema)):
+    def __init__(self, token: str = Depends(auth_schema), db: Session = Depends(get_db)):
         log.debug(f"verifying token: {token.credentials}")
         self.credentials = token.credentials
+        self.db = db
 
     def auth(self, model_required_permissions):
         try:
             verified = auth_provider.verify(self.credentials)
+            log.info("SET zekoder.id")
+            self.db.execute(f"SET zekoder.id = '{verified.id}'")
         except:
             raise HTTPException(401, "user not authenticated or using invalid token")
 
-        return True
-        raise HTTPException(403, "user not authorized to do this action")
+        return verified
