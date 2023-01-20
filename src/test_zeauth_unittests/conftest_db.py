32940b732d4d2ad38470f128bb6d107c19c0c253
Alper Uygur <a.uygur@cyberneticlabs.io>
2023-01-20 14:04:23 +0300

    Unit testing

---
+++ src/test_zeauth_unittests/conftest_db.py
@@ -0,0 +1,35 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+class Settings:
+    POSTGRES_USER: str = os.environ.get("POSTGRES_USER")
+    POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+    POSTGRES_SERVER: str = os.environ.get("POSTGRES_SERVER")
+    POSTGRES_PORT: str = os.environ.get("POSTGRES_PORT", 5432)
+    POSTGRES_DB: str = os.environ.get("POSTGRES_TEST_DB")   # For unit testing
+    DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+
+SQLALCHEMY_DATABASE_URL = Settings().DATABASE_URL
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def override_get_db():
+    db = TestingSessionLocal()
+    # db.execute("SET zekoder.id = '09c645f2-8353-11ed-80ca-571e9bc0bf17'")
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+
+
+
---
+++ src/test_zeauth_unittests/test_root.py
@@ -0,0 +1,18 @@
+import pytest
+from httpx import AsyncClient
+from starlette.status import HTTP_200_OK
+from api import app
+
+
+class TestRoot:
+    @pytest.mark.asyncio
+    async def test_root(self):  # root
+        async with AsyncClient(app=app, base_url="http://localhost:8080/") as ac:
+            res = await ac.get("/")
+        assert res.status_code == HTTP_200_OK
+        assert res.json() == {'message': 'ZeKoder Security Management API'}
+
+
+
+
+
